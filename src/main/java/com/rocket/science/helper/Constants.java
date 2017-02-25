969b549f20a3a6ed37351f1c15710f19970c0c96
Aadithya Hatwar Hosabettu <aadithya.hatwar@olacabs.com>
2017-02-25 23:33:36 +0530

    Helper Service for http and constants

---
+++ src/main/java/com/rocket/science/helper/Constants.java
@@ -0,0 +1,54 @@
+package com.rocket.science.helper;
+
+public class Constants {
+
+    public enum DriverStatus {
+        IDLE("IDLE"),
+        IN_TRIP("IN_TRIP"),
+        LOGGED_OUT("LOGGED_OUT");
+
+        private String driverStatus;
+
+        DriverStatus(String driverStatus){
+            this.driverStatus = driverStatus;
+        }
+
+        public String getText() {
+            return this.driverStatus;
+        }
+
+        public String toString(){
+            return this.driverStatusHtt.toString();
+        }
+
+    }
+
+
+    public enum BookingStatus {
+        BOOKING_REQUEST("BOOKING_REQUEST"),
+        REACHED_FOR_PICKUP("REACHED_FOR_PICKUP"),
+        WAITING_FOR_CLIENT("WAITING_FOR_CLIENT"),
+        TRIP_STARTED("TRIP_STARTED"),
+        TRIP_ENDED("TRIP_ENDED"),
+        TRIP_CANCELLED("TRIP_CANCELLED"),
+        STOCK_OUT("STOCK_OUT");
+
+        private String bookingStatus;
+
+        BookingStatus(String bookingStatus){
+            this.bookingStatus = bookingStatus;
+        }
+
+        public String getText() {
+            return this.bookingStatus;
+        }
+
+        public String toString(){
+            return this.bookingStatus.toString();
+        }
+
+    }
+
+
+
+}
---
+++ src/main/java/com/rocket/science/helper/HttpClientHelper.java
@@ -0,0 +1,113 @@
+package com.rocket.science.helper;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Optional;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.json.JSONObject;
+
+
+
+public class HttpClientHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpClientHelper.class);
+
+
+    public Optional<JSONObject> getRequest(String urlString){
+
+
+        try{
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpGet getRequest = new HttpGet(urlString);
+            getRequest.addHeader("accept", "application/json");
+
+            HttpResponse response = httpClient.execute(getRequest);
+
+            if (response.getStatusLine().getStatusCode() != 200) {
+
+                LOGGER.error("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+            }
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+
+            String output;
+
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            JSONObject res = new JSONObject(output);
+
+            httpClient.getConnectionManager().shutdown();
+
+            return Optional.of(res);
+
+        }
+        catch(Exception e){
+
+            return Optional.empty();
+        }
+
+    }
+
+
+    public Optional<JSONObject> postRequest(String url, JSONObject jsonobject){
+
+        try{
+            HttpClient client = new DefaultHttpClient();//HttpClientBuilder.create().build();
+            HttpPost request = new HttpPost(url);
+            StringEntity params =new StringEntity(jsonobject.toString());
+            request.addHeader("content-type", "application/x-www-form-urlencoded");
+            request.setEntity(params);
+            HttpResponse response = client.execute(request);
+
+            if(response.getStatusLine().getStatusCode()!=200){
+                LOGGER.error("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+            }
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+
+            String output;
+
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            JSONObject res = new JSONObject(output);
+
+            client.getConnectionManager().shutdown();
+
+            return Optional.of(res);
+
+
+        }
+        catch(Exception e){
+
+
+            return Optional.empty();
+
+        }
+
+
+    }
+
+}
